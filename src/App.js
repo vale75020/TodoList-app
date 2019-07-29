2f074b48486e767ed6c118499b9c5312d13d01dd
vale75020 <valentinosuma@hotmail.com>
2019-07-29 17:24:27 +0200

    add todo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import React, {Component} from 'react';
-import Todos from './Todos'
+import React, { Component } from "react";
+import Todos from "./Todos";
+import AddForm from "./AddForm";
 
 class App extends Component {
-  state= {
-    todos: [
-      {id: 1, content: "buy milk"},
-      {id: 2, content: "buy bread"},
-    ]
-  }
+  state = {
+    todos: [{ id: 1, content: "buy milk" }, { id: 2, content: "buy bread" }]
+  };
 
-  deleteTodo = (id) => {
-    console.log(id)
+  deleteTodo = id => {
+    console.log(id);
     const updateTodos = this.state.todos.filter(todo => {
-      return todo.id !== id
-    })
+      return todo.id !== id;
+    });
     this.setState({
       todos: updateTodos
+    });
+  };
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let newTodosArray = [...this.state.todos, todo];
+    this.setState({
+      todos: newTodosArray
     })
   }
 
   render() {
-  return (
-    <div className="todos-app container">
-      <h1 className="center blue-text">Todo's</h1>
-      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-    </div>
-  );
-}
+    return (
+      <div className="todos-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddForm addTodo={this.addTodo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
